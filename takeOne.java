226a56dfe052a5556f483b9de959d14df4e883c5
rachelli23 <rkanounji23@gmail.com>
2022-04-27 12:43:09 -0700

    Added a test file

--- takeOne.java
+++ takeOne.java
@@ -1,5 +1,8 @@
 public class takeOne{
 
+    public static int multiple(int n,int m){
+        return n * m; 
+    }
 
 
 
---
+++ takeOneTest.java
@@ -0,0 +1,11 @@
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class takeOneTest {
+    @Test
+    public void multiplication() {
+        assertEquals(13, takeOne.multiple(3,4));
+    }
+
+}
